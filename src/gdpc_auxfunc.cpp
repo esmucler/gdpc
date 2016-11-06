esmucler/gdpc

commit e92b845ecf0bebabbc9795f8a5de6f279b3c3fc3

* Missing () in several input checks * Check for correct range of num_comp in fitted.gdpcs * Use 'reconstruction' instead of 'fitted model' in man pages * More readable input arrangement in c++ code * More comments in c++ code * Use unsafe element access

src/gdpc_auxfunc.cpp
@@ -4,9 +4,27 @@ using namespace arma;
 // [[Rcpp::depends(RcppArmadillo)]]
 
 
-void getMatrixBeta(const arma::mat & Z, const arma::vec & f, const int & k, const int & sel, arma::mat & betaOut, arma::mat & resOut, double & mseOut, double & critOut) {
+void getMatrixBeta(const arma::mat & Z, 
+                   const arma::vec & f, 
+                   const int & k, 
+                   const int & sel, 
+                   arma::mat & betaOut, 
+                   arma::mat & resOut, 
+                   double & mseOut, 
+                   double & critOut) {
   // This function finds the optimal beta, the residuals, mse and criterion corresponding to Z, f and k.
   // alpha is the last column of beta.
+  // INPUT
+  // Z: data matrix each ROW is a different time series
+  // f: principal component
+  // k: number of leads used
+  // sel: LOO (1), AIC (2), BIC (3), BNG (4)
+  // OUTPUT
+  // betaOut: matrix of loadings and intercept corresponding to f. Last column are the
+  // intercepts (alpha).
+  // resOut: matrix of residuals
+  // mseOut:  mean squared error (in N and m)
+  // critOut: criterion used to evaluate the fit
   int m = Z.n_rows;
   int N = Z.n_cols;
   arma::mat Fmat = mat(k + 1, N);
@@ -28,7 +46,7 @@ void getMatrixBeta(const arma::mat & Z, const arma::vec & f, const int & k, cons
   resOut = Z - betaOut * Fmat;
   mseOut = accu(pow(resOut, 2)) / N;
   if (sel == 1) {
-    arma::vec weights = 1/(1 - diagvec(Proj));
+    arma::vec weights = 1 / (1 - diagvec(Proj));
     critOut = accu( pow(resOut * diagmat(weights) , 2) ) / (N * m);
   } else if (sel == 2) {
     critOut = N * log(mseOut) + m * (k + 2) * 2;  
@@ -40,7 +58,9 @@ void getMatrixBeta(const arma::mat & Z, const arma::vec & f, const int & k, cons
   mseOut = mseOut / m;
 }
 
-arma::mat getMatrixC(const arma::subview_row<double> & rowZ, const double & alpha, const int & k) {
+arma::mat getMatrixC(const arma::subview_row<double> & rowZ,
+                     const double & alpha,
+                     const int & k) {
   // Constructs the matrix C correspoding to rowZ, alpha and k 
   int N = rowZ.n_elem;
   arma::mat C = zeros(N + k, k + 1);
@@ -52,15 +72,17 @@ arma::mat getMatrixC(const arma::subview_row<double> & rowZ, const double & alph
     liml[1] = t - N;
     limu[1] = t - 1;
     for (int q = 1; q <= k + 1; q++){
-      if( (q >= max(liml) + 1) & (q<= min(limu) + 1)){
-        C(t - 1, q - 1) = rowZ(t - q) - alpha;
+      if( (q >= max(liml) + 1) & (q <= min(limu) + 1)){
+        C.at(t - 1, q - 1) = rowZ[t - q] - alpha;
       }
     }
   }
   return (C);
 }
 
-arma::mat getMatrixD(const arma::subview_row<double> & rowbeta, const int & N, const int & k) {
+arma::mat getMatrixD(const arma::subview_row<double> & rowbeta,
+                     const int & N,
+                     const int & k) {
   // Constructs the matrix D corresponding to rowbeta, N and k 
   arma::mat beta_mat = zeros(N + k, N + k);
   arma::vec liml = vec(2);
@@ -72,22 +94,24 @@ arma::mat getMatrixD(const arma::subview_row<double> & rowbeta, const int & N, c
     limu[0] = t;
     for (int r = max(liml); r <= min(limu); r++){
       for (int q = r; q <= r + k; q++){
-        beta_mat(t - 1, q - 1) = beta_mat(t - 1, q - 1) + rowbeta(q - r) * rowbeta(t - r);
+        beta_mat.at(t - 1, q - 1) += rowbeta[q - r] * rowbeta[t - r];
       }
     }
   }
   return (beta_mat);
 }
 
-arma::vec getF(const arma::mat & Z, const arma::mat & beta, const int & k) {
+arma::vec getF(const arma::mat & Z,
+               const arma::mat & beta,
+               const int & k) {
   // Get optimal f corresponding to Z, beta, alpha and k.
   int m = Z.n_rows;
   int N = Z.n_cols;
   arma::mat D = zeros(N + k, N + k);
   arma::vec fOut = zeros(N + k, 1);
-  for (int j=0 ; j < m ; j++){
-    D = D + getMatrixD(beta(j, span(0, k)), N, k);
-    fOut = fOut + getMatrixC(Z.row(j), beta(j, k + 1), k) * (beta(j, span(0, k))).t();
+  for (int j = 0 ; j < m ; j++){
+    D += getMatrixD(beta(j, span(0, k)), N, k);
+    fOut += getMatrixC(Z.row(j), beta(j, k + 1), k) * (beta(j, span(0, k))).t();
   }
   
   double condition_D = rcond(D);
@@ -101,7 +125,11 @@ arma::vec getF(const arma::mat & Z, const arma::mat & beta, const int & k) {
 }
 
 // [[Rcpp::export]]
-arma::mat getFitted(arma::vec & f_fin, const arma::vec & f_ini, const arma::mat & beta, const arma::vec & alpha, const int & k) {
+arma::mat getFitted(arma::vec & f_fin,
+                    const arma::vec & f_ini,
+                    const arma::mat & beta,
+                    const arma::vec & alpha,
+                    const int & k) {
   // Get fitted values associated with f and beta and alpha
   int N = f_fin.n_elem;
   if (k > 0){
@@ -119,7 +147,8 @@ arma::mat getFitted(arma::vec & f_fin, const arma::vec & f_ini, const arma::mat 
 }
 
 // [[Rcpp::export]]
-arma::vec getFini(const arma::mat & Z, const int & k) {
+arma::vec getFini(const arma::mat & Z,
+                  const int & k) {
   // Get initial estimator: ordinary principal component with k leads
   int N = Z.n_cols;
   arma::vec f_iniOut = zeros(N + k, 1);
@@ -140,7 +169,13 @@ arma::vec getFini(const arma::mat & Z, const int & k) {
 
 
 // [[Rcpp::export]]
-List gdpc_priv(const arma::mat & Z, const int & k, const arma::vec & f_ini, const bool & passf_ini,const double & tol, const int & niter_max, const int & sel) {
+List gdpc_priv(const arma::mat & Z,
+               const int & k,
+               const arma::vec & f_ini,
+               const bool & passf_ini,
+               const double & tol,
+               const int & niter_max,
+               const int & sel) {
 // This function computes a single GDPC with a given number of leads.
 // INPUT
 // Z: data matrix each ROW is a different time series

R/classGDPC.R
@@ -150,7 +150,7 @@ plot.gdpc <- function(x, which = "Component", which_load = 0, ...) {
   }
   if (!inherits(which_load, "numeric")) {
     stop("which_load should be numeric")
-  } else if (any(!which_load == floor(which_load), which_load < 0, which_load > ncol(x$beta) - 1)) {
+  } else if (any(!(which_load == floor(which_load)), which_load < 0, which_load > ncol(x$beta) - 1)) {
     stop("which_load should be a non-negative integer, at most equal to the number of lags")
   }
   if (which == "Component"){
@@ -238,7 +238,7 @@ components.gdpcs <- function(object, which_comp = 1) {
   }
   if (all(!inherits(which_comp, "numeric"), !inherits(which_comp, "integer"))) {
     stop("which_comp should be numeric")
-  } else if (any(!which_comp == floor(which_comp), which_comp <= 0, which_comp > length(object))) {
+  } else if (any(!(which_comp == floor(which_comp)), which_comp <= 0, which_comp > length(object))) {
     stop("The entries of which_comp should be positive integers, at most equal to the number of components")
   }
   object <- object[which_comp]
@@ -256,6 +256,11 @@ fitted.gdpcs <- function(object, num_comp = 1, ...) {
   # Returns the fitted values of a gdpcs object using components 1,...,num_comp
   if (!is.gdpcs(object)) {
     stop("object should be of class gdpcs")
+  }
+  if (all(!inherits(num_comp, "numeric"), !inherits(num_comp, "integer"))) {
+    stop("num_comp should be numeric")
+  } else if (any(!(num_comp == floor(num_comp)), num_comp <= 0, num_comp > length(object))) {
+    stop("num_comp should be a positive integer, at most equal to the number of components")
   }
   fitted <- Reduce('+', lapply(object[1:num_comp], fitted))
   if (inherits(object[[1]]$f, "xts")) {
@@ -278,7 +283,7 @@ plot.gdpcs <- function(x, which_comp = 1, ...) {
   }
   if (all(!inherits(which_comp, "numeric"), !inherits(which_comp, "integer"))) {
     stop("which_comp should be numeric")
-  } else if (any(!which_comp == floor(which_comp), which_comp <= 0, which_comp > length(x))) {
+  } else if (any(!(which_comp == floor(which_comp)), which_comp <= 0, which_comp > length(x))) {
     stop("The entries of which_comp should be positive integers, at most equal to the number of components")
   }
   comps <- components(x, which_comp)

man/auto.gdpc.Rd
@@ -24,7 +24,7 @@ Computes Generalized Dynamic Principal Components. The number of components can 
 \details{
 
 Suppose the data matrix consists of \eqn{m} series of length \eqn{T}.
-Let \eqn{\bold{f}} be the Dynamic Principal Component defined using \eqn{k} lags, let \eqn{R} be the corresponding matrix of residuals and let \eqn{\Sigma = (R^{\prime} R) / T}.
+Let \eqn{\bold{f}} be the dynamic principal component defined using \eqn{k} lags, let \eqn{R} be the corresponding matrix of residuals and let \eqn{\Sigma = (R^{\prime} R) / T}.
 
 If crit = 'LOO' the number of lags is chosen among \eqn{0,\dots, k_{max}} as the value \eqn{k} that minimizes the leave-one-out (LOO) cross-validation mean squared error, given by
 \deqn{ LOO = \frac{1}{T m}\sum\limits_{i=1}^{m}\sum\limits_{t=1}^{T}\frac{R_{t,i}^{2}}{(1-h_{t,t})^{2}},}
@@ -48,12 +48,12 @@ If normalize = 1, the data is analyzed in the original units, without mean and v
   An object of class \code{gdpcs}, that is, a list of length equal to the number of computed components. The i-th entry of this list is an object of class \code{gdpc}, that is, a list with entries
    \item{expart}{Proportion of the variance explained by the first i components.}
   \item{mse}{Mean squared error of the reconstruction using the first i components.}
-  \item{crit}{The value of the criterion of the fitted model, according to what the user specified.}
+  \item{crit}{The value of the criterion of the reconstruction, according to what the user specified.}
   \item{k}{Number of lags chosen.}
   \item{alpha}{Vector of intercepts corresponding to f.}
   \item{beta}{Matrix of loadings corresponding to f. Column number \eqn{k} is the vector of \eqn{k-1} lag loadings.}
-  \item{f}{Coordinates of the i-th Principal Component corresponding to the periods \eqn{1,\dots,T}.}
-  \item{initial_f}{Coordinates of the i-th Principal Component corresponding to the periods \eqn{-k+1,\dots,0}. Only for the case \eqn{k>0}, otherwise 0.}
+  \item{f}{Coordinates of the i-th dynamic principal component corresponding to the periods \eqn{1,\dots,T}.}
+  \item{initial_f}{Coordinates of the i-th dynamic principal component corresponding to the periods \eqn{-k+1,\dots,0}. Only for the case \eqn{k>0}, otherwise 0.}
       \item{call}{The matched call.}
       \item{conv}{Logical. Did the iterations converge?}
   \code{components}, \code{fitted}, \code{plot} and \code{print} methods are available for this class.

man/gdpc.Rd
@@ -26,12 +26,12 @@ See \code{\link{auto.gdpc}} for the definition of criterion that is part of the 
   An object of class \code{gdpc}, that is, a list with entries:
    \item{expart}{Proportion of the variance explained.}
  \item{mse}{Mean squared error.}
- \item{crit}{The value of the criterion of the fitted model, according to what the user specified.}
+ \item{crit}{The value of the criterion of the reconstruction, according to what the user specified.}
  \item{k}{Number of lags used.}
  \item{alpha}{Vector of intercepts corresponding to f.}
    \item{beta}{Matrix of loadings corresponding to f. Column number \eqn{k} is the vector of \eqn{k-1} lag loadings.}
- \item{f}{Coordinates of the first principal component corresponding to the periods \eqn{1,\dots,T}.}
-  \item{initial_f}{Coordinates of the first principal component corresponding to the periods \eqn{-k+1,\dots,0}. Only for the case \eqn{k>0}, otherwise 0.}
+ \item{f}{Coordinates of the first dynamic principal component corresponding to the periods \eqn{1,\dots,T}.}
+  \item{initial_f}{Coordinates of the first dynamic principal component corresponding to the periods \eqn{-k+1,\dots,0}. Only for the case \eqn{k>0}, otherwise 0.}
        \item{call}{The matched call.}
        \item{conv}{Logical. Did the iterations converge?}
  \code{fitted}, \code{plot} and \code{print} methods are available for this class.

commit 2ecab53c8120524601f03b363a8732e6c85d0aba

Fixed a bug in gdpc_auxfunc introduced in commit e92b845

src/gdpc_auxfunc.cpp
@@ -94,7 +94,7 @@ arma::mat getMatrixD(const arma::subview_row<double> & rowbeta,
     limu[0] = t;
     for (int r = max(liml); r <= min(limu); r++){
       for (int q = r; q <= r + k; q++){
-        beta_mat.at(t - 1, q - 1) += rowbeta[q - r] * rowbeta[t - r];
+        beta_mat.at(t - 1, q - 1) = beta_mat.at(t - 1, q - 1) + rowbeta[q - r] * rowbeta[t - r];
       }
     }
   }
@@ -110,8 +110,8 @@ arma::vec getF(const arma::mat & Z,
   arma::mat D = zeros(N + k, N + k);
   arma::vec fOut = zeros(N + k, 1);
   for (int j = 0 ; j < m ; j++){
-    D += getMatrixD(beta(j, span(0, k)), N, k);
-    fOut += getMatrixC(Z.row(j), beta(j, k + 1), k) * (beta(j, span(0, k))).t();
+    D = D + getMatrixD(beta(j, span(0, k)), N, k);
+    fOut = fOut + getMatrixC(Z.row(j), beta(j, k + 1), k) * (beta(j, span(0, k))).t();
   }
   
   double condition_D = rcond(D);